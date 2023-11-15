7dd0e97fd0a9db2cab7a0e1afe1cfb9b831eaca2
Ebuka Ezenwabasili <wizzyblaq94@gmail.com>
2023-11-15 09:31:24 +0100

    home page for app cards

---
+++ src/pages/Home.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { PRODUCTS } from "../utils/data";
+import FilterableProductsTable from "../components/FilterableProductsTable";
+import WIndowsSize from "../components/WIndowsSize";
+import Card from "../components/Card";
+import { FaBeer, FaBlog, FaProductHunt, FaAddressBook } from "react-icons/fa";
+
+const Home = () => {
+  return (
+    <section className="home">
+      {/* <h1>Select Your Preffered APP</h1> */}
+      <div className="app_container">
+        <div className="grid_container">
+          <Card
+            title={"Product Filter App"}
+            onClick={() => {}}
+            icon={<FaProductHunt width={120} height={120} />}
+          />
+          <Card
+            title={"Contact Search App"}
+            link={"/contact"}
+            onClick={() => {}}
+            icon={<FaAddressBook width={120} height={120} />}
+          />
+          <Card
+            title={"Blog Post App"}
+            onClick={() => {}}
+            link={"/blog"}
+            icon={<FaBlog width={40} height={40} />}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
