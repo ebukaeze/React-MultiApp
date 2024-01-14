90ac239ef730988067c25a29c25f8ebfbb311c0d
Ebuka Ezenwabasili <wizzyblaq94@gmail.com>
2024-01-14 14:53:15 +0100

    chore: added delete contact

--- src/routes/destroy.jsx
+++ src/routes/destroy.jsx
@@ -2,6 +2,17 @@ import { redirect } from "react-router-dom";
 import { deleteContact } from "../contacts";
 
 export async function action({ params }) {
+  //throw new Error("Oh dang!");
   await deleteContact(params.contactId);
   return redirect("/contacts");
+}
+
+export default function DeleteContact() {
+  return (
+    <>
+      <div>
+        <p>Contact Deleted Successfully</p>
+      </div>
+    </>
+  );
 }
