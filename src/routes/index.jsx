d5d7561b7233d031026af714d3a244cacaaf09dd
Ebuka Ezenwabasili <wizzyblaq94@gmail.com>
2024-01-14 14:55:02 +0100

    chore: added desc to index for seo

---
+++ src/routes/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function IndexPage() {
+  return (
+    <>
+      <img
+        src="https://wiki.teamfortress.com/w/images/thumb/5/5c/Painted_One-Man_Army_E6E6E6.png/125px-Painted_One-Man_Army_E6E6E6.png"
+        alt="Jaggy"
+      />
+      <br />
+      <p id="zero-state" style={{ fontSize: "23px" }}>
+        Hello! I'm Jaggy and This is a demo for React Router.
+        <br />
+        Check out{" "}
+        <a href="https://reactrouter.com">the docs at reactrouter.com</a>.
+      </p>
+    </>
+  );
+}
